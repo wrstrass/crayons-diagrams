8e2c7c85695511dceb8a1409e8a2a445a5e6051a
Anton <wrstrass@gmail.com>
2023-06-04 20:43:11 +0300

    Fix config

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+PREFIX = "/api/v1"
+
 MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_PORT = os.getenv("MONGO_PORT")
 MONGO_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
