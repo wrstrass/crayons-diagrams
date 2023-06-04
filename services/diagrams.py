ca3bc3709b55ae7fbca1f0258045db7cf778b063
Anton <wrstrass@gmail.com>
2023-06-04 21:48:26 +0300

    Add get_diagram_info

--- services/diagrams.py
+++ services/diagrams.py
@@ -4,7 +4,17 @@ from schemas import NameDescriptionSchema, DiagramSchema
 
 class DiagramsService:
     @staticmethod
-    async def new(data: NameDescriptionSchema) -> NameDescriptionSchema:
+    async def new(data: NameDescriptionSchema) -> DiagramSchema:
         diagram = DiagramModel(**data.dict())
         await diagram.save()
         return DiagramSchema.from_model(diagram)
+
+    @staticmethod
+    async def get(oid: str) -> DiagramSchema:
+        diagram = await DiagramModel.get_by_id(oid)
+        return DiagramSchema.from_model(diagram)
+
+    @staticmethod
+    async def get_shapes(oid: str) -> DiagramSchema:
+        diagram = await DiagramModel.get_by_id(oid)
+        return diagram.shapes
--- views/diagrams.py
+++ views/diagrams.py
@@ -9,3 +9,11 @@ router = APIRouter(prefix="/diagrams")
 @router.post("/")
 async def new_diagram(data: NameDescriptionSchema):
     return await DiagramsService.new(data)
+
+@router.get("/{diagram_oid}/")
+async def get_diagram(diagram_oid: str):
+    return await DiagramsService.get(diagram_oid)
+
+@router.get("/{diagram_oid}/shapes")
+async def get_diagram_shapes(diagram_oid: str):
+    return await DiagramsService.get_shapes(diagram_oid)
